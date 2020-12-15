c128c45e90a95f5129afaa48aeb8c6630406b821
Philo Kaulkin <Phil0nator@users.noreply.github.com>
2020-12-14 21:30:47 -0800

    better error fix

--- Classes/Error.py
+++ Classes/Error.py
@@ -24,7 +24,10 @@ class Error:
         
         
         lines = file.split("\n")
+        # determine number of characters before error token on given line
         line -= lines[0] != ""
+        beginchars = lines[line-1].find(self.tok.value)
+
         lines[line-1] = lines[line-1].replace(self.tok.value, f"{error_indicator}{self.tok.value}{Style.RESET_ALL}", 1)
         lp = ""
         try:
@@ -34,8 +37,7 @@ class Error:
                 lp += f"|{line}\t" + lines[line-1] + "\n"
         except IndexError:
             pass
-        # determine number of characters before error token on given line
-        beginchars = lp.rfind("\n", -1, 0)
+        
         # highlight problem token
         problem = lp
         # add underline
