42c2f74de80e7378f8b260eb94fec6bdabd89fa9
Philo Kaulkin <48894630+Phil0nator@users.noreply.github.com>
2021-01-09 20:33:31 -0800

    bugfixes and update osx installer

--- install.sh
+++ install.sh
@@ -36,25 +36,72 @@ python3 -m pip install cpuid
 
 
 
-# now system dependant operations will be done depending
-# on weather the system is MacOs or Linux GNU
+# linux_installer contains all of the linux-specific operations
+function linux_installer(){
 
-if ["$(uname)" == "Darwin"]; then
-  # MacOs X
-  brew install nasm
-
-
-
-
-else
-  # Linux GNU
-  # satisfy assembler dependency 
+  # handle assembler dependency
   sudo apt-get install nasm
 
-fi
 
   mkdir /usr/lib/cbm
   # copy files
   cp -a -r -v * /usr/lib/cbm
   # create symbolic link for terminal command
   ln -s /usr/lib/cbm/cbm.sh /usr/bin/cbm
+}
+
+# the osx_installer contains all of the osx-specific operations
+function osx_installer(){
+  
+  # handle assembler dependency
+  which nasm
+  # if nasm isn't installed:
+  if [ "$0" -ne 0 ]; then
+    
+    which brew
+    # if brew isn't installed:
+    if [ "$0" -ne 0 ]; then 
+      echo "Please either install NASM (Netwide Assembler) yourself, or install brew so that this script can install it."
+      exit;
+    fi
+    # install nasm with brew
+    sudo -u $SUDO_USER "brew" "install" "nasm";
+
+  fi
+
+  # create a bin folder in home
+  mkdir ~/bin
+  # create a source folder for CBM
+  mkdir ~/bin/cbmsrc
+  # copy the project
+  cp -v -R -p * ~/bin/cbmsrc
+  # create a symbolic link in /usr/local/bin called cbm for the terminal command
+  ln -s ~/bin/cbmsrc/cbm.sh /usr/local/bin/cbm
+  
+
+  # update the bash script to work on osx
+  echo '
+  #!/usr/bin/env bash
+  python3 ~/bin/cbmsrc/compile.py "$@"
+  ' >> ~/bin/cbmsrc/cbm.sh
+  
+  # setup executable permissions
+  # (755 means executable by normal users, and root)
+  chmod 755 ~/bin/cbmsrc/cbm.sh
+  chmod 755 /usr/local/bin/cbm
+
+
+}
+
+
+
+# now system dependant operations will be done depending
+# on weather the system is MacOs or Linux GNU
+case "$OSTYPE" in
+  solaris*) echo "SOLARIS" ;;
+  darwin*)  osx_installer ;; 
+  linux*)   linux_installer ;;
+  bsd*)     echo "BSD" ;;
+  msys*)    echo "WINDOWS" ;;
+  *)        echo "unknown: $OSTYPE" ;;
+esac
